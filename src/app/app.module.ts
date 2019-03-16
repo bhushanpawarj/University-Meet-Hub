c6d19656278926c6210c7b219de404979ea81baa
Bhushan Pawar <bhushanpawar@mail.fresnostate.edu>
2019-03-16 16:21:58 -0700

    added components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { FollowUserComponent } from './follow-user/follow-user.component';
+import { HomeComponent } from './home/home.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent,
+    FollowUserComponent,
+    HomeComponent,
+    MyProfileComponent,
+    ProfileComponent,
+    SidebarComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule
